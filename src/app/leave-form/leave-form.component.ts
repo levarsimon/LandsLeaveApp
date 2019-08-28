a9c7ed92db48715998080fc179e2d51166751be4
Levar Simon <zerokoolgy@yahoo.com>
2019-08-28 16:28:05 -0400

    Aug 28 Second Commit (Working on Date Validation)

--- src/app/leave-form/leave-form.component.ts
+++ src/app/leave-form/leave-form.component.ts
@@ -11,6 +11,8 @@ export class LeaveFormComponent implements OnInit {
 
   requestForm: FormGroup;
   feedback: Leave;
+  currentDate: Date = new Date();
+ 
   @ViewChild('rform',{static:false}) requestFormDirective;
   
   constructor(private formBuilder: FormBuilder) {
@@ -25,9 +27,36 @@ export class LeaveFormComponent implements OnInit {
       reason: new FormControl('', Validators.compose([Validators.required, Validators.minLength(8)])),
       startDate: new FormControl('', Validators.required),
       endDate: new FormControl('', Validators.required)
-    });
+    }, {validator: this.dateBeforeCheck('currentDate', 'startDate')},//{validator: this.dateLessThan('startDate', 'endDate')},
+    );
   }
 
+  dateLessThan(from: string, to: string) {
+    return (group: FormGroup): {[key: string]: any} => {
+      let f = group.controls[from];
+      let t = group.controls[to];
+      if (f.value > t.value) {
+        return {
+          dates: "Start Date should be less than End Date"
+        };
+      }
+      return {};
+    }
+  }
+
+  dateBeforeCheck(currentDate: String, startDate: String) {
+    return (group: FormGroup): {[key: string]: any} => {
+      let d1 = group.controls[currentDate];
+      let d2 = group.controls[startDate];
+      if (d1.value > d2.value) {
+        return {
+          dates: "Start Date has already passed"
+        };
+      }
+      return{};
+    }
+  }
+  
   onSubmit() {
     this.feedback = this.requestForm.value;
     this.requestForm.reset({
